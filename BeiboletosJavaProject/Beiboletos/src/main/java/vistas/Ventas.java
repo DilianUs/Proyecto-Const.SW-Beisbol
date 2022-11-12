17c4c0604d8666eb2056f3f255b0a5bc7234261c
DilianUs <dilian0217@gmail.com>
2022-11-12 16:23:32 -0600

    nombres de variables de botones

--- BeiboletosJavaProject/Beiboletos/src/main/java/vistas/Ventas.java
+++ BeiboletosJavaProject/Beiboletos/src/main/java/vistas/Ventas.java
@@ -30,11 +30,16 @@ public class Ventas extends javax.swing.JFrame {
         beiboletos1 = new com.mycompany.beiboletos.Beiboletos();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        btnInicio = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        btnPartidos = new javax.swing.JButton();
+        btnEquipos = new javax.swing.JButton();
+        btnVentas = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -45,38 +50,51 @@ public class Ventas extends javax.swing.JFrame {
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Inicio");
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 150, 40));
+        btnInicio.setText("Inicio");
+        jPanel2.add(btnInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 150, 40));
 
         jLabel1.setBackground(new java.awt.Color(0, 0, 0));
         jLabel1.setForeground(new java.awt.Color(51, 51, 51));
         jLabel1.setText("Logo");
         jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 50, 90, 50));
 
-        jButton2.setText("Partidos");
-        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 240, 150, 40));
+        btnPartidos.setText("Partidos");
+        jPanel2.add(btnPartidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 240, 150, 40));
 
-        jButton3.setText("Equipos");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        btnEquipos.setText("Equipos");
+        btnEquipos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                btnEquiposActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 310, 150, 40));
+        jPanel2.add(btnEquipos, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 310, 150, 40));
 
-        jButton4.setText("Ventas");
-        jPanel2.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 390, 150, 40));
+        btnVentas.setText("Ventas");
+        jPanel2.add(btnVentas, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 390, 150, 40));
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 300, 750));
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel2.setText("Reporte");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 50, -1, -1));
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 120, 700, 350));
+
+        jLabel3.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel3.setText("Registro");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 510, -1, -1));
+        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 550, 650, 80));
+        jPanel1.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 640, 650, 70));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1200, 750));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void btnEquiposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEquiposActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_btnEquiposActionPerformed
 
     /**
      * @param args the command line arguments
@@ -118,12 +136,17 @@ public class Ventas extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.mycompany.beiboletos.Beiboletos beiboletos1;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton btnEquipos;
+    private javax.swing.JButton btnInicio;
+    private javax.swing.JButton btnPartidos;
+    private javax.swing.JButton btnVentas;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
     // End of variables declaration//GEN-END:variables
 }
--- BeiboletosJavaProject/Beiboletos/src/main/java/vistas/inicio.java
+++ BeiboletosJavaProject/Beiboletos/src/main/java/vistas/inicio.java
@@ -30,11 +30,11 @@ public class inicio extends javax.swing.JFrame {
         beiboletos1 = new com.mycompany.beiboletos.Beiboletos();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        jButton1 = new javax.swing.JButton();
+        btnInicio = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        btnPartidos = new javax.swing.JButton();
+        btnEquipos = new javax.swing.JButton();
+        btnVentas = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
@@ -57,27 +57,27 @@ public class inicio extends javax.swing.JFrame {
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setText("Inicio");
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 150, 40));
+        btnInicio.setText("Inicio");
+        jPanel2.add(btnInicio, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 150, 40));
 
         jLabel1.setBackground(new java.awt.Color(0, 0, 0));
         jLabel1.setForeground(new java.awt.Color(51, 51, 51));
         jLabel1.setText("Logo");
         jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 50, 90, 50));
 
-        jButton2.setText("Partidos");
-        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 240, 150, 40));
+        btnPartidos.setText("Partidos");
+        jPanel2.add(btnPartidos, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 240, 150, 40));
 
-        jButton3.setText("Equipos");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        btnEquipos.setText("Equipos");
+        btnEquipos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                btnEquiposActionPerformed(evt);
             }
         });
-        jPanel2.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 310, 150, 40));
+        jPanel2.add(btnEquipos, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 310, 150, 40));
 
-        jButton4.setText("Ventas");
-        jPanel2.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 390, 150, 40));
+        btnVentas.setText("Ventas");
+        jPanel2.add(btnVentas, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 390, 150, 40));
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 300, 750));
 
@@ -132,9 +132,9 @@ public class inicio extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void btnEquiposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEquiposActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_btnEquiposActionPerformed
 
     /**
      * @param args the command line arguments
@@ -173,10 +173,10 @@ public class inicio extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.mycompany.beiboletos.Beiboletos beiboletos1;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
+    private javax.swing.JButton btnEquipos;
+    private javax.swing.JButton btnInicio;
+    private javax.swing.JButton btnPartidos;
+    private javax.swing.JButton btnVentas;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JComboBox<String> jComboBox1;
