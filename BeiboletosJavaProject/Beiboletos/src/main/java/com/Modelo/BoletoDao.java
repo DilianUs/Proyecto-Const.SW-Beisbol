53e44799584143aa1f13534e936e498f6797ad87
JETC-98 <blackhayatebo@hotmail.com>
2022-11-12 18:01:57 -0600

    Agregado BoletoDAO

---
+++ BeiboletosJavaProject/Beiboletos/src/main/java/com/Modelo/BoletoDao.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Modelo;
+
+/**
+ *
+ * @author black
+ */
+public class BoletoDao {
+    private String ClvBoleto;
+    private String ClvPartido;
+
+    public BoletoDao(String ClvBoleto, String ClvPartido) {
+        this.ClvBoleto = ClvBoleto;
+        this.ClvPartido = ClvPartido;
+    }
+    
+    
+    
+    public void agregarBoleto(){
+        
+    }
+    
+    public void buscarBoleto(){
+        
+    }
+    
+    public void eliminarBoleto(){
+        
+    }
+}
